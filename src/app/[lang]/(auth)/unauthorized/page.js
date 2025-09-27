f28451c9b86905e7b1ae8e0fdf5ba36cf2ba75ec
rawda <arwrw872@gmail.com>
2025-09-27 20:44:38 +0300

    Unauthorized page added

--- src/app/[lang]/(auth)/unauthorized/page.js
+++ src/app/[lang]/(auth)/unauthorized/page.js
@@ -1,22 +1,21 @@
+import { getTranslations } from "next-intl/server";
 import Link from "next/link";
-import { getDictionary } from '../../dictionaries.js'
 
-export default async function UnauthorizedPage({params}) {
-  const { lang } = await params
-  const dict = await getDictionary(lang)
+export default async function UnauthorizedPage() {
+  const dict = await getTranslations('unauthorized');
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-white">
       <div className="flex items-center space-x-4">
         <h1 className="border-r pr-4 text-2xl font-semibold text-black ml-10">401</h1>
         <div className="text-gray-700">
-          <p className="text-lg">{dict.unauthorized.error}</p>
+          <p className="text-lg">{dict("error")}</p>
         </div>
       </div>
-      <Link href={`/${lang}/login`} className="text-white mt-7 py-3 px-6 bg-black inline-block rounded-md text-lg hover:border-2 hover:bg-white hover:text-black hover:border-black">
-        {dict.unauthorized.loginTitle}
+      <Link href={`/login`} className="text-white mt-7 py-3 px-6 bg-black inline-block rounded-md text-lg hover:border-2 hover:bg-white hover:text-black hover:border-black">
+        {dict("loginTitle")}
       </Link>
-      <Link href={`/${lang}/`} className="text-gray-600 mt-2 px-6 inline-block rounded-md text-sm hover:text-black">
-        {dict.unauthorized.goHome}
+      <Link href={`/`} className="text-gray-600 mt-2 px-6 inline-block rounded-md text-sm hover:text-black">
+        {dict("goHome")}
       </Link>
     </div>
   );
